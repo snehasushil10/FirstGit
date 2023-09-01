39d804b2d1f53a67fe25ff8657cffabda71f4509
snehasushil10 <snehasushil10@gmail.com>
2023-09-01 14:14:44 +0530

    getelementbyclass

---
+++ letsgetelementbyclassname/task3.js
@@ -0,0 +1,6 @@
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor ="green";
+items[0].style.fontWeight = "bold";
+items[1].style.fontWeight = "bold";
+items[2].style.fontWeight = "bold";
+items[3].style.fontWeight = "bold";
